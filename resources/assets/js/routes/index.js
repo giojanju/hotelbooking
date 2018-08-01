63509aa286532d2769d2ff015ee3e99811b0cc61
giojanju <giorgi.janjulia@gtu.ge>
2018-08-01 22:34:51 +0400

    Fix to redirect route scroll not top

--- resources/assets/js/routes/index.js
+++ resources/assets/js/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ScrollToTop from '../components/ScrollToTop';
 
 import routes from './routes'
 
 const Routes = () => (
     <Switch>
-        {routes.map((route, i) => {
-            return <Route key={i} path={route.path} exact={route.exact} component={route.component} />
-        })}
+    	<ScrollToTop>
+	        {routes.map((route, i) => {
+	            return <Route key={i} path={route.path} exact={route.exact} component={route.component} />
+	        })}
+	    </ScrollToTop>
     </Switch>
 );
 
